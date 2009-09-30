49f859f570a6cc8e9b23616ffddf38c1f959e044
joycem137 <joycem137@b45f98dc-4754-0410-b1c4-6d8b6d76e98d>
2009-09-30 07:19:11 +0000

    Moved all graphical logic from Card into CardComponent.

--- src/gjset/engine/Card.java
+++ src/gjset/engine/Card.java
@@ -28,16 +28,6 @@ package gjset.engine;
  *  along with gjSet.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Polygon;
-import java.awt.Shape;
-import java.awt.geom.Path2D;
-import java.awt.geom.RoundRectangle2D;
-import java.awt.image.BufferedImage;
-
 public class Card
 {
 	// Store constants for handling the various properties of the cards.
@@ -58,20 +48,6 @@ public class Card
 	public static final int	PROP_COLOR			= 2;
 	public static final int	PROP_SHADING		= 3;
 	public static final int	PROP_SHAPE			= 4;
-
-	private Image			image;						// The card's image.
-
-	// The width and height of the cards are currently fixed. This may be changed later.
-	public static final int	CARD_WIDTH			= 100;
-	public static final int	CARD_HEIGHT			= 139;
-
-	// Symbol sizes are currently set statically.
-	private final int		SHAPE_WIDTH			= 70;
-	private final int		SHAPE_HEIGHT		= 30;
-
-	// Set the shadow offset.
-	private final int		CARD_SHADOW_OFFSET	= 3;
-	private final int		SHAPE_SHADOW_OFFSET	= 2;
 
 	// Store the various properties of this card.
 	private int				number, color, shading, shape;
@@ -94,178 +70,6 @@ public class Card
 		this.color = color;
 		this.shading = shading;
 		this.shape = shape;
-
-		// Generate the actual card image.
-		generateCardImage();
-	}
-
-	// Draws the image of the card for use later.
-	public void generateCardImage()
-	{
-		// Create the image object to store the drawing of the card.
-		image = new BufferedImage(CARD_WIDTH + CARD_SHADOW_OFFSET, CARD_HEIGHT + CARD_SHADOW_OFFSET,
-				BufferedImage.TYPE_INT_ARGB_PRE);
-
-		// Get the image's graphics context.
-		Graphics2D g = ((BufferedImage) image).createGraphics();
-
-		// Draw card shadow
-		g.setColor(Color.black);
-		g.fillRoundRect(CARD_SHADOW_OFFSET, CARD_SHADOW_OFFSET, CARD_WIDTH, CARD_HEIGHT, 15, 15);
-
-		// Draw card background.
-		g.setColor(Color.white);
-		g.fillRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, 15, 15);
-
-		// Draw card outline.
-		g.setColor(Color.black);
-		g.setStroke(new BasicStroke(1.0f));
-		g.drawRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, 15, 15);
-
-		// Determine where the first symbol should start, based on the number of symbols.
-		int topSymbolY = getTopYPosition();
-
-		// Draw each symbol.
-		for (int i = 0; i < number; i++)
-		{
-			// Draw the shadow
-			g.setColor(Color.black);
-			drawSymbol(g, 15 + SHAPE_SHADOW_OFFSET, topSymbolY + i * 44 + SHAPE_SHADOW_OFFSET);
-
-			// Draw the actual color.
-			g.setColor(getDrawingColor());
-			drawSymbol(g, 15, topSymbolY + i * 44);
-		}
-
-		// Flush the image drawing.
-		image.flush();
-	}
-
-	// Draws the symbol at the currently specified location.
-	private void drawSymbol(Graphics2D g, int x, int y)
-	{
-		Shape drawingShape = null;
-
-		// Determine which shape we're going to draw.
-		switch (shape)
-		{
-			case SHAPE_OVAL:
-				// The oval is a simple round rectangle.
-				drawingShape = new RoundRectangle2D.Double(x, y, SHAPE_WIDTH, SHAPE_HEIGHT, 25.0, 25.0);
-				break;
-			case SHAPE_SQUIGGLE:
-				/*
-				 * The squiggle is drawn by doing a complicated path using lines and quadratic curves.
-				 */
-				Path2D.Double squiggle = new Path2D.Double();
-				squiggle.moveTo(x, y);
-				squiggle.lineTo(x, y + SHAPE_HEIGHT - 5);
-				squiggle.curveTo(x + 23, y + SHAPE_HEIGHT + 20, x + 46, y + SHAPE_HEIGHT - 30, x + SHAPE_WIDTH, y
-						+ SHAPE_HEIGHT);
-				squiggle.lineTo(x + SHAPE_WIDTH, y + 5);
-				squiggle.curveTo(x + 46, y - 20, x + 23, y + 30, x, y);
-
-				drawingShape = squiggle;
-				break;
-			case SHAPE_DIAMOND:
-				// The diamond is a simple polygon.
-				Polygon diamond = new Polygon();
-				diamond.addPoint(x, y + SHAPE_HEIGHT / 2);
-				diamond.addPoint(x + SHAPE_WIDTH / 2, y);
-				diamond.addPoint(x + SHAPE_WIDTH, y + SHAPE_HEIGHT / 2);
-				diamond.addPoint(x + SHAPE_WIDTH / 2, y + SHAPE_HEIGHT);
-
-				drawingShape = diamond;
-				break;
-		}
-
-		// Actually draw the shape.
-		g.setStroke(new BasicStroke(4.0f));
-		g.draw(drawingShape);
-
-		// Now handle shading the shape.
-		if (shading == SHADING_FULL)
-		{
-			// Fill the entire shape.
-			g.fill(drawingShape);
-		}
-		else if (shading == SHADING_STRIPED)
-		{
-			// Set the width of the lines to be drawn.
-			g.setStroke(new BasicStroke(2.0f));
-
-			// Move across the object, left to right.
-			for (int xPos = x + 6; xPos < x + SHAPE_WIDTH; xPos += 6)
-			{
-				// Identify the top and bottom of the shape.
-				int topY = -1;
-				int bottomY = -1;
-
-				// Crawl along each pixel to determine if it is inside the shape or not.
-				for (int yPos = y; yPos < y + SHAPE_HEIGHT; yPos++)
-				{
-					// Check to see if this is above our current estimate for the top.
-					if (topY < 0)
-					{
-						if (drawingShape.contains(xPos, yPos))
-						{
-							topY = yPos;
-						}
-					}
-					else if (bottomY < 0) // Do the same for the bottom.
-					{
-						if (!drawingShape.contains(xPos, yPos))
-						{
-							bottomY = yPos - 1;
-						}
-					}
-				}
-
-				// Now draw the line
-				if (bottomY > 0)
-				{
-					// If we found the bottom, draw from the top to the bottom.
-					g.drawLine(xPos, topY, xPos, bottomY);
-				}
-				else if (topY > 0)
-				{
-					// Sometimes the bottom IS the bottom of the object. Draw to there.
-					g.drawLine(xPos, topY, xPos, y + SHAPE_HEIGHT);
-				}
-			}
-		}
-	}
-
-	// Identify the y position of the top symbol, based on the number of symbols to be drawn.
-	private int getTopYPosition()
-	{
-		switch (number)
-		{
-			case 1:
-				return 54;
-			case 2:
-				return 31;
-			case 3:
-				return 10;
-			default:
-				return 0;
-		}
-	}
-
-	// Convert the color property to a Color object.
-	private Color getDrawingColor()
-	{
-		switch (color)
-		{
-			case COLOR_RED:
-				return Color.red;
-			case COLOR_BLUE:
-				return Color.blue;
-			case COLOR_GREEN:
-				return Color.green;
-			default:
-				return Color.black;
-		}
 	}
 
 	public int getNumber()
@@ -330,9 +134,6 @@ public class Card
 			// Abort if we come across an unfulfilled value.
 			if (getProperty(p2) == 0) return;
 		}
-
-		// Generate the card image.
-		generateCardImage();
 	}
 
 	/**
@@ -399,10 +200,5 @@ public class Card
 		boolean result = (card.color == color && card.shading == shading && card.shape == shape && card.number == number);
 		//System.out.println("Comparing " + this.toString() + " with " + card.toString() + ": " + result);
 		return result; 
-	}
-
-	public Image getImage()
-	{
-		return image;
 	}
 }
--- src/gjset/gui/CardComponent.java
+++ src/gjset/gui/CardComponent.java
@@ -36,12 +36,30 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Polygon;
+import java.awt.Shape;
+import java.awt.geom.Path2D;
 import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 
 public class CardComponent extends JComponent
 {
+
+	// The width and height of the cards are currently fixed. This may be changed later.
+	public static final int	CARD_WIDTH			= 100;
+	public static final int	CARD_HEIGHT			= 139;
+
+	// Symbol sizes are currently set statically.
+	private final int		SHAPE_WIDTH			= 70;
+	private final int		SHAPE_HEIGHT		= 30;
+
+	// Set the shadow offset.
+	private final int		CARD_SHADOW_OFFSET	= 3;
+	private final int		SHAPE_SHADOW_OFFSET	= 2;
+	
 	/**
 	 * 
 	 */
@@ -50,11 +68,13 @@ public class CardComponent extends JComponent
 	private boolean				highlighted;				// Indicates whether this card is highlighted.
 
 	private Card				card;						// Stores the card data for this card.
+	private Image				image;						// Stores the image of the card
 
 	// Create a basic blank card.
 	public CardComponent(GameController gameController, Card card)
 	{
 		this.card = card;
+		generateCardImage();
 
 		// Create the mouse listener for this object
 		addMouseListener(new CardMouseListener(this, gameController));
@@ -63,8 +83,177 @@ public class CardComponent extends JComponent
 		highlighted = false;
 
 		// Set the height and width of the card.
-		setMaximumSize(new Dimension(card.getImage().getWidth(this), card.getImage().getHeight(this)));
-		setMinimumSize(new Dimension(card.getImage().getWidth(this), card.getImage().getHeight(this)));
+		setMaximumSize(new Dimension(image.getWidth(this), image.getHeight(this)));
+		setMinimumSize(new Dimension(image.getWidth(this), image.getHeight(this)));
+	}
+
+	// Draws the image of the card for use later.
+	private void generateCardImage()
+	{
+		// Create the image object to store the drawing of the card.
+		image = new BufferedImage(CARD_WIDTH + CARD_SHADOW_OFFSET, CARD_HEIGHT + CARD_SHADOW_OFFSET,
+				BufferedImage.TYPE_INT_ARGB_PRE);
+
+		// Get the image's graphics context.
+		Graphics2D g = ((BufferedImage) image).createGraphics();
+
+		// Draw card shadow
+		g.setColor(Color.black);
+		g.fillRoundRect(CARD_SHADOW_OFFSET, CARD_SHADOW_OFFSET, CARD_WIDTH, CARD_HEIGHT, 15, 15);
+
+		// Draw card background.
+		g.setColor(Color.white);
+		g.fillRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, 15, 15);
+
+		// Draw card outline.
+		g.setColor(Color.black);
+		g.setStroke(new BasicStroke(1.0f));
+		g.drawRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, 15, 15);
+
+		// Determine where the first symbol should start, based on the number of symbols.
+		int topSymbolY = getTopYPosition();
+
+		// Draw each symbol.
+		for (int i = 0; i < card.getNumber(); i++)
+		{
+			// Draw the shadow
+			g.setColor(Color.black);
+			drawSymbol(g, 15 + SHAPE_SHADOW_OFFSET, topSymbolY + i * 44 + SHAPE_SHADOW_OFFSET);
+
+			// Draw the actual color.
+			g.setColor(getDrawingColor());
+			drawSymbol(g, 15, topSymbolY + i * 44);
+		}
+
+		// Flush the image drawing.
+		image.flush();
+	}
+
+	// Draws the symbol at the currently specified location.
+	private void drawSymbol(Graphics2D g, int x, int y)
+	{
+		Shape drawingShape = null;
+
+		// Determine which shape we're going to draw.
+		switch (card.getShape())
+		{
+			case Card.SHAPE_OVAL:
+				// The oval is a simple round rectangle.
+				drawingShape = new RoundRectangle2D.Double(x, y, SHAPE_WIDTH, SHAPE_HEIGHT, 25.0, 25.0);
+				break;
+			case Card.SHAPE_SQUIGGLE:
+				/*
+				 * The squiggle is drawn by doing a complicated path using lines and quadratic curves.
+				 */
+				Path2D.Double squiggle = new Path2D.Double();
+				squiggle.moveTo(x, y);
+				squiggle.lineTo(x, y + SHAPE_HEIGHT - 5);
+				squiggle.curveTo(x + 23, y + SHAPE_HEIGHT + 20, x + 46, y + SHAPE_HEIGHT - 30, x + SHAPE_WIDTH, y
+						+ SHAPE_HEIGHT);
+				squiggle.lineTo(x + SHAPE_WIDTH, y + 5);
+				squiggle.curveTo(x + 46, y - 20, x + 23, y + 30, x, y);
+
+				drawingShape = squiggle;
+				break;
+			case Card.SHAPE_DIAMOND:
+				// The diamond is a simple polygon.
+				Polygon diamond = new Polygon();
+				diamond.addPoint(x, y + SHAPE_HEIGHT / 2);
+				diamond.addPoint(x + SHAPE_WIDTH / 2, y);
+				diamond.addPoint(x + SHAPE_WIDTH, y + SHAPE_HEIGHT / 2);
+				diamond.addPoint(x + SHAPE_WIDTH / 2, y + SHAPE_HEIGHT);
+
+				drawingShape = diamond;
+				break;
+		}
+
+		// Actually draw the shape.
+		g.setStroke(new BasicStroke(4.0f));
+		g.draw(drawingShape);
+
+		// Now handle shading the shape.
+		if (card.getShading() == Card.SHADING_FULL)
+		{
+			// Fill the entire shape.
+			g.fill(drawingShape);
+		}
+		else if (card.getShading() == Card.SHADING_STRIPED)
+		{
+			// Set the width of the lines to be drawn.
+			g.setStroke(new BasicStroke(2.0f));
+
+			// Move across the object, left to right.
+			for (int xPos = x + 6; xPos < x + SHAPE_WIDTH; xPos += 6)
+			{
+				// Identify the top and bottom of the shape.
+				int topY = -1;
+				int bottomY = -1;
+
+				// Crawl along each pixel to determine if it is inside the shape or not.
+				for (int yPos = y; yPos < y + SHAPE_HEIGHT; yPos++)
+				{
+					// Check to see if this is above our current estimate for the top.
+					if (topY < 0)
+					{
+						if (drawingShape.contains(xPos, yPos))
+						{
+							topY = yPos;
+						}
+					}
+					else if (bottomY < 0) // Do the same for the bottom.
+					{
+						if (!drawingShape.contains(xPos, yPos))
+						{
+							bottomY = yPos - 1;
+						}
+					}
+				}
+
+				// Now draw the line
+				if (bottomY > 0)
+				{
+					// If we found the bottom, draw from the top to the bottom.
+					g.drawLine(xPos, topY, xPos, bottomY);
+				}
+				else if (topY > 0)
+				{
+					// Sometimes the bottom IS the bottom of the object. Draw to there.
+					g.drawLine(xPos, topY, xPos, y + SHAPE_HEIGHT);
+				}
+			}
+		}
+	}
+
+	// Identify the y position of the top symbol, based on the number of symbols to be drawn.
+	private int getTopYPosition()
+	{
+		switch (card.getNumber())
+		{
+			case 1:
+				return 54;
+			case 2:
+				return 31;
+			case 3:
+				return 10;
+			default:
+				return 0;
+		}
+	}
+
+	// Convert the color property to a Color object.
+	private Color getDrawingColor()
+	{
+		switch (card.getColor())
+		{
+			case Card.COLOR_RED:
+				return Color.red;
+			case Card.COLOR_BLUE:
+				return Color.blue;
+			case Card.COLOR_GREEN:
+				return Color.green;
+			default:
+				return Color.black;
+		}
 	}
 
 	// Actually paint the card.
@@ -73,7 +262,7 @@ public class CardComponent extends JComponent
 		Graphics2D g2 = (Graphics2D) g;
 
 		// Draw the card image to the screen.
-		g.drawImage(card.getImage(), 0, 0, this);
+		g.drawImage(image, 0, 0, this);
 
 		// Draw the highlight
 		if (highlighted)
@@ -81,7 +270,7 @@ public class CardComponent extends JComponent
 			// Draw the main highlight.
 			g.setColor(Color.yellow);
 			g2.setStroke(new BasicStroke(6.3f));
-			g2.draw(new RoundRectangle2D.Double(0, 0, Card.CARD_WIDTH, Card.CARD_HEIGHT, 15, 15));
+			g2.draw(new RoundRectangle2D.Double(0, 0, CARD_WIDTH, CARD_HEIGHT, 15, 15));
 		}
 	}
 
